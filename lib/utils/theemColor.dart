b6cdbf494562d657372c674ca9e0c7975ba15963
AfaqX <xdeveloper0@gmail.com>
2024-03-08 20:31:45 +0500

    add

---
+++ lib/utils/theemColor.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+class AppColors {
+  static const primaryColor = Color.fromARGB(255, 233, 7, 7);
+  static const accentColor = Color(0xFFFFD600);
+  static const backgroundColor = Color(0xFFF5F5F5);
+  static const textColor = Color(0xFF333333);
+  static const buttonColorred = Color.fromARGB(255, 201, 6, 6);
+}
